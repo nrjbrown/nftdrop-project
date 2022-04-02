754570826ee993953e86cfe666c780ed63432fa9
Blockchain Dev <nevillebrown_930@hotmail.com>
2022-04-02 10:10:59 -0400

    Day2 Completed

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>NFT App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="">Welcome to the NFT Drop Challenge</h1>
+    </div>
+  )
+}
+
+export default Home
